ead1e9cc96491e5071c5b1826ebb71a63dc092db
Kyle Pfister <kpfister44@gmail.com>
2025-05-09 14:36:00 -0500

    Initial Commit

---
+++ WorkoutTimer/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer
+//
+//  Created by Kyle Pfister on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutTimerTests/WorkoutTimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutTimerTests.swift
+//  WorkoutTimerTests
+//
+//  Created by Kyle Pfister on 5/9/25.
+//
+
+import Testing
+
+struct WorkoutTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
